3ff1a26421737b9385d81576cc2a5a74b0a0c7bc
wenkaifan0720 <wenkai@flutterflow.io>
2024-09-04 10:49:12 -0400

    actually update the devtool web app

--- extension/devtools/build/flutter_service_worker.js
+++ extension/devtools/build/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "d214e461614e7c786de6847972018398",
-"version.json": "0a388a4d54ecb5fc56daaf2a8e2b7923",
+const RESOURCES = {"flutter_bootstrap.js": "d005e9d2120e7718cca937260add64a8",
+"version.json": "c23de8a747ef489dc455a052ef4526f9",
 "index.html": "aa6f466ba4f6154a3e7ad6465ec3796b",
 "/": "aa6f466ba4f6154a3e7ad6465ec3796b",
-"main.dart.js": "560f433b6755beb94995b04bbea745e4",
+"main.dart.js": "64089f467e8b94802161facda552ed99",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
