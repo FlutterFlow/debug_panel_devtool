3b126206af1ddcbfb859eac4d159b47989094685
Souvik Biswas <sbis1999@gmail.com>
2024-12-14 02:21:31 +0530

    Expose a few more files (#7)
    * Export narrow modal
    
    * Export a few more widgets

--- lib/src/widgets/flutter_flow_widgets.dart
+++ lib/src/widgets/flutter_flow_widgets.dart
@@ -1,3 +1,6 @@
 export 'action_text.dart';
 export 'debug_variables_panel.dart';
 export 'filter_icon_button.dart';
+export 'narrow_modal.dart';
+export 'node_widget.dart';
+export 'debug_tree_view.dart';
