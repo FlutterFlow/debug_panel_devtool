550545c1cd03515509d717de78510a7d51e86111
wenkaifan0720 <wenkai@flutterflow.io>
2025-05-23 13:45:37 -0400

    update intl

--- extension/devtools/build/flutter_bootstrap.js
+++ extension/devtools/build/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "251000066"
+    serviceWorkerVersion: "3709924447"
   }
 });
